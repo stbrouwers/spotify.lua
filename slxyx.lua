f9469af42f85d123f3e733c59bd702f05d38aca5
irritetech <74833242+irritetech@users.noreply.github.com>
2021-03-10 15:39:44 +0000

    Update slxyx.lua

--- slxyx.lua
+++ slxyx.lua
@@ -14,14 +14,17 @@ end
 
 IsNotAuthed = true
 
-local function AuthoriseFunc()
+local function GetAuthKeyFunc()
     authkey = CP()
+end
+
+local function Refresh()
     http.get("https://api.spotify.com/v1/me/player?market=GB&access_token="..authkey, function(success, response)
         if not success or response.status ~= 200 then client.error_log("Failed to authenticate with Spotify servers."); IsNotAuthed = true; return end
         spotidata = json.parse(response.body)
         IsNotAuthed = false
     end)
-    http.get("https://api.spotify.com/v1/me?market=GB&access_token=BQB-fa6Sov2AP7qsZ-U8mi-U23P5CedUXjYNHHMR5I0CVabCqcsJ0AF0xXf0WREMxPUe3snhgO55JNFwWYmCNBVoAUAX5NteXyQVmuvSFVX5_bUq0Baeqi_TU_kNzTLoVeQuHHd1gs9GMttVurypA03rJoRcov78Gsc4rHT45IM", function(success, response)
+    http.get("https://api.spotify.com/v1/me?market=GB&access_token="..authkey, function(success, response)
         if not success or response.status ~= 200 then client.error_log("Failed to authenticate with Spotify servers."); IsNotAuthed = true; return end
         spotidata2 = json.parse(response.body)
         IsNotAuthed = false
@@ -30,14 +33,16 @@ end
 
 local labelcol = ui.new_label("MISC", "Miscellaneous", "Spotify Progress Colour")
 local col = ui.new_color_picker("MISC", "Miscellaneous", "Spotify Progress Colourr", 0, 255, 0, 255)
-local auth = ui.new_button("MISC", "Miscellaneous", "Authorise", AuthoriseFunc)
+local getkey = ui.new_button("MISC", "Miscellaneous", "Get auth key from clipboard", GetAuthKeyFunc)
+local auth = ui.new_button("MISC", "Miscellaneous", "Authorise", Refresh)
 
 local last_update = client.unix_time()
 client.set_event_callback("paint_ui", function()
     ui.set_visible(auth, IsNotAuthed)
-    if not spotidata and not spotidata2 then return end
+    if spotidata == nil then return end
+    if spotidata2 == nil then return end
     if client.unix_time() > last_update + 1 then
-        AuthoriseFunc()
+        Refresh()
         last_update = client.unix_time()
     end
     if spotidata.is_playing and renderer.measure_text("b", "Now Playing: "..spotidata.item.name.." by "..spotidata.item.artists[1].name)+10 > renderer.measure_text("b", "Connected to: "..spotidata2.display_name)+10 then
@@ -48,7 +53,7 @@ client.set_event_callback("paint_ui", function()
         textmeasurement = renderer.measure_text("b", "Connected to: "..spotidata2.display_name)+10
     end
     r, g, b, a = ui.get(col)
-    renderer.gradient(0, 70, textmeasurement, 32, 0, 22, 22, 255, 22, 22, 22, 10, true)
+    renderer.gradient(0, 70, textmeasurement, 32, 22, 22, 22, 255, 22, 22, 22, 10, true)
     renderer.rectangle(0, 70, 2, 32, r, g, b, a)
     renderer.gradient(0, 70, spotidata.progress_ms/spotidata.item.duration_ms*textmeasurement, 2, r, g, b, a, r, g, b, 0, true)
     renderer.gradient(0, 100, spotidata.progress_ms/spotidata.item.duration_ms*textmeasurement, 2, r, g, b, a, r, g, b, 0, true)
